09ddb5cca2c5a0e66ac41dae1b6b9bc73bc9be71
Mekhi Rhodie <46280016+Mekhi-Rhodie@users.noreply.github.com>
2020-02-29 13:37:41 -0500

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,18 @@
+
+
 new Vue({
     el: "main",
     data: {
-        characters: 75,
+        message: "",
+        maxcharacter: 75,
+        remaincharactersText: "",
         message: "",
         todo: "",
         todos: [],
         todosCompleted: [],
         input: "",
         checked: false,
-        time: new Date().toLocaleTimeString()
+        time: "",
     },
     methods: {
         reference: function () {
@@ -18,14 +22,25 @@ new Vue({
             this.message = ""
             console.log(this.todos)
             console.log(this.time)
+            this.characters = 76
+            this.time = new Date().toLocaleTimeString()
         },
         closeToDo: function (todo) {
             const todoIndex = this.todos.indexOf(todo);
             this.todos.splice(todoIndex, 1);
         },
-        todoComplete: function(todo) {
+        todoComplete: function (todo) {
             this.todosCompleted.push(todo)
             console.log(this.todosCompleted)
+        },
+        characterCount: function () {
+            if (this.message.length > this.maxcharacter) {
+                this.remaincharactersText = "Exceeded " + this.maxcharacter + " characters limit.";
+            } else {
+
+                var remainCharacters = this.maxcharacter - this.message.length;
+                this.remaincharactersText = remainCharacters;
+            }
         }
     }
 })
